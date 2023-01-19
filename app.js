36c0d08388ba6a14e2a649730af58e504ee446b3
Esquiafo <leocapone@hotmail.com>
2023-01-18 22:51:51 -0300

    init

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.post("/create_preference", (req, res) => {
     .create(preference)
     .then(function (response) {
         console.log(response)
-     res.redirect(response.body.sandbox_init_point)
+     res.redirect(response.body.init_point)
     })
     .catch(function (error) {
       console.log(error);
